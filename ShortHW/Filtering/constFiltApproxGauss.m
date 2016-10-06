eb085bb09bf7e459ee39f0cda53b48445020a907
ospiro <Oliver@1x-nat-vl930-172-30-97-200.wireless.umass.edu>
2016-10-06 19:42:40 -0400

    Dir renaming

--- ShortHW/Filtering/constFiltApproxGauss.m
+++ ShortHW/Filtering/constFiltApproxGauss.m
@@ -1,25 +1,25 @@
-% im = imread('ca-nn-dmsc.jpg','jpg');
-% im = im2double(im);
-% im = im(:,:,1);
+clear
+im = imread('cat.jpg','jpg');
+im = im2double(im);
+im = im(:,:,1);
 normfactor = 1/(2*pi);
 gaussFilt = zeros(5,5);
 
 %create gaussian filter
-for i = 1:5
-    for j = 1:5
-        x = j-3;
-        y = 3-i;
+for i = 1:9
+    for j = 1:9
+        x = j-41;
+        y = 41-i;
         gaussFilt(i,j) = normfactor*exp(-0.5*(x^2 + y^2));
     end
 end
-% imFiltGauss = filter2(gaussFilt,im,'same');
-
-%loop to compare i iterations of const filter with gauss filter
-constFilt = (1/25)*ones(5,5);
+%loop to compare i iterations of const filter with gauss filter. Pad array
+%with zeros to avoid boundary problems with 'full'.
+constFilt = (1/81)*ones(9,9);
 repConstFilt = constFilt;
 resids = zeros(100);
 for i = 1:100
-    repConstFilt = filter2(repConstFilt,constFilt,'same');
+    repConstFilt = filter2(constFilt,repConstFilt,'same');
     sum(sum((repConstFilt-gaussFilt).^2))
 end
 % figure
